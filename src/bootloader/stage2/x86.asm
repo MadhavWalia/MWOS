c7e953d73d048dcfe6007b07cc658949eba58eff
MadhavWalia <madyboysupreme@LAPTOP-JS1O1255>
2023-09-12 13:29:02 +0530

    Defined the 2 part division function

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -4,6 +4,48 @@ section _TEXT class=CODE
 
 ; Written in lieu of the _cdecl calling convention
 
+
+;
+; void _cdecl x86_div64_32
+; args: dividend (64-bit), divisor (32-bit)
+; returns: quotient (64-bit), remainder (32-bit)
+;
+global _x86_div64_32
+_x86_div64_32:
+    ; making a new stack frame (since we are in small memory mode, only the offset is needed)
+    push bp                 ; save the old bp
+    mov bp, sp              ; initialize the frame
+
+    push bx                 ; saving bx
+
+    ; Dividing in 32-bit chunks (using long division)
+    mov eax, [bp + 8]       ; eax <- upper 32 bits of dividend (Because the value is stored in little endian)
+    mov ecx, [bp + 12]      ; ecx <- divisor
+    xor edx, edx            ; clearing edx
+
+    div ecx                 ; eax <- quotient, edx <- remainder
+
+    ; Saving the quotient (upper 32 bits)
+    mov bx, [bp + 16]      ; (16-bit register required due to near pointer)
+    mov [bx + 4], eax      
+
+    ; Dividing the lower 32 bits
+    mov eax, [bp + 4]       ; eax <- lower 32 bits of dividend
+    div ecx                 ; eax <- quotient, edx <- remainder
+
+    ; Saving the quotient (lower 32 bits)
+    mov [bx], eax
+    mov bx, [bp + 18]
+    mov [bx], edx
+
+    pop bx                  ; restoring bx
+
+    ; restoring the old stack frame
+    mov sp, bp              ; restore the stack pointer
+    pop bp                  ; restore the old bp
+    ret
+
+
 ;
 ; int 10h / ah=0Eh
 ; args: characters, page
