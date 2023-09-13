e25bd9e726d51cf5f99c849f85ac96ec7cd22f23
MadhavWalia <madyboysupreme@LAPTOP-JS1O1255>
2023-09-13 11:22:49 +0530

    Added the heads for the assembly function wrappers to read, reset, and fetch parameters using BIOS interrupts

--- src/bootloader/stage2/x86.h
+++ src/bootloader/stage2/x86.h
@@ -4,3 +4,22 @@
 void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotient, uint32_t* remainder);
 
 void _cdecl x86_video_writeChar(char c, uint8_t page);
+
+bool _cdecl x86_Disk_Reset(uint8_t drive);
+
+bool _cdecl x86_Disk_Read(
+  uint8_t drive, 
+  uint16_t cylinder, 
+  uint16_t head, 
+  uint16_t sector, 
+  uint8_t count, 
+  uint8_t far* dataOut,
+);
+
+bool _cdecl x86_Disk_GetDriveParameters(
+  uint8_t drive, 
+  uint8_t* driveTypeOut, 
+  uint16_t* cylindersOut, 
+  uint16_t* sectorsOut, 
+  uint16_t* headsOut,
+);
